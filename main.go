86da88ae25886d8d7920c64b41f48c70ccdeb9f1
obscurity <z@x4.pm>
2022-05-21 14:31:11 +0200

    no duplicates

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ type Song struct {
 	File string `json:"file,omitempty"`
 }
 
+type Asset struct {
+	Name string
+}
+
 func main() {
 	np, err := http.Get("http://localhost:" + os.Getenv("PORT") + "/NP")
 	if err != nil {
@@ -32,10 +36,26 @@ func main() {
 	fmt.Printf("%s - %s\n", music.Artist(), music.Album())
 	b64, md5 := encode(music.Picture().Data)
 	fmt.Println(md5)
+
+	url := "https://discord.com/api/v9/oauth2/applications/" + os.Getenv("APP_ID") + "/assets"
+	assetsRes, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer assetsRes.Body.Close()
+	assetsBody, _ := io.ReadAll(assetsRes.Body)
+	var assets []Asset
+	json.Unmarshal(assetsBody, &assets)
+	for _, asset := range assets {
+		if asset.Name == md5 {
+			fmt.Println("artwork already uploaded")
+			os.Exit(0)
+		}
+	}
+	req := gorequest.New()
 	b64img := "data:image/jpeg;base64," + b64
 	payload := fmt.Sprintf(`{"name": "%s", "image": "%s", "type": "1"}`, md5, b64img)
-	req := gorequest.New()
-	_, body, _ := req.Post("https://discord.com/api/v9/oauth2/applications/"+os.Getenv("APP_ID")+"/assets").
+	_, body, _ := req.Post(url).
 		Set("Authorization", os.Getenv("TOKEN")).
 		Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36").
 		Set("origin", "discordapp.com").
